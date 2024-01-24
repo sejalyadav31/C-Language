825d8044f1d0a89c64a14b10e03a346eb81a546a
Sejal Yadav <156797408+sejalyadav31@users.noreply.github.com>
2024-01-24 17:55:40 +0530

    Add files via upload

---
+++ lec 5 2.c
@@ -0,0 +1,33 @@
+#include<stdio.h> // header file
+main()
+
+{
+	// wap to find min. no. from given 2 no.
+	int a,b;
+	printf("Enter a&b:");
+	scanf("%d%d",&a,&b);
+	
+	if(a<b)
+	{
+		printf("A is Minimum !");
+		
+	}
+	else if(b<a)
+	{
+		printf("B is Minimum !");
+		
+	}
+	else if(a==b)
+	{
+		printf("A and B are Equal !");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
