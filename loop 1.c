741a3571e3ce655ca23964404cc70dbf24fdaf26
Sejal Yadav <156797408+sejalyadav31@users.noreply.github.com>
2024-02-01 18:42:21 +0530

    Add files via upload

---
+++ loop 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>// header file
+main()
+{
+	int x=1;
+	
+	while(x<=10)
+	{
+		printf("%d\n",x);
+		x++;
+		
+	}
+}
---
+++ loop 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>// header file
+main()
+{
+	int x=10;
+	
+	while(x>=1)
+	{
+		printf("%d\n",x);
+		x--;
+		
+	}
+}
